a1177ae3632630af6f732d4a8b5da394a2b58661
aliencindy <lucy8611892@gmail.com>
2023-03-14 22:13:16 +0800

    SharedPreferences記住上次成功登入的帳號

--- app/src/main/java/com/chrissy/atm/LoginActivity.java
+++ app/src/main/java/com/chrissy/atm/LoginActivity.java
@@ -19,6 +19,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG =LoginActivity.class.getSimpleName();
     private EditText edAccount;
     private EditText edPwd;
 
@@ -28,9 +29,25 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
+        // 創建喜好檔(練習)
+//        getSharedPreferences("atm",MODE_PRIVATE)
+//                .edit()
+//                .putInt("LEVEL",3) // 設定關卡第三關
+//                .putString("Name","Chrissy")
+//                .commit();
+
+        //讀取喜好檔(練習)
+//        int level = getSharedPreferences("atm",MODE_PRIVATE)
+//                .getInt("LEVEL",0);
+//        Log.d(TAG, "onCreate: "+level);
 
         edAccount = findViewById(ed_account);
         edPwd = findViewById(R.id.ed_pwd);
+
+        // 如果上次成功登入，那帳號會直接出現在edAccount
+        String accountPref = getSharedPreferences("atm",MODE_PRIVATE)
+                .getString("account","");// 沒有值的話就不用顯示
+        edAccount.setText(accountPref);
 
     }
 
@@ -49,6 +66,11 @@ public class LoginActivity extends AppCompatActivity {
                         // 登入成功
                         if (fbPwd!=null && fbPwd.equals(pwd)){
                             Log.d("LoginActivity","onDataChange: "+fbPwd);
+                            // 記住使用者帳號
+                            getSharedPreferences("atm",MODE_PRIVATE).edit()
+                                    .putString("account",account)
+                                    .apply();
+
                             setResult(RESULT_OK);
                             finish();//結束此activity,回到上一層。
                         }else {
@@ -74,7 +96,6 @@ public class LoginActivity extends AppCompatActivity {
 
                     }
                 });
-
 
 //        if (a.equals("hi") && p.equals("1234")) {
 //            setResult(RESULT_OK);
