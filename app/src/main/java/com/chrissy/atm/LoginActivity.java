1a2c45d2b75933e9f27b85edcb06ba3b9b12c76c
aliencindy <lucy8611892@gmail.com>
2023-03-14 14:26:37 +0800

    導入Firebase、帳密從DB讀取、登入錯誤處理

--- app/src/main/java/com/chrissy/atm/LoginActivity.java
+++ app/src/main/java/com/chrissy/atm/LoginActivity.java
@@ -1,16 +1,21 @@
 package com.chrissy.atm;
 
-import static android.content.ContentValues.TAG;
-import static com.chrissy.atm.MainActivity.REQUEST_LOGIN;
 import static com.chrissy.atm.R.id.ed_account;
 
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -31,13 +36,50 @@ public class LoginActivity extends AppCompatActivity {
 
 
     public void login(View view) {
-        String a = edAccount.getText().toString();
-        String p = edPwd.getText().toString();
+        String account = edAccount.getText().toString();
+        String pwd = edPwd.getText().toString();
 
-        if (a.equals("hi") && p.equals("1234")) {
-            setResult(RESULT_OK);
-            finish();//結束此activity,回到上一層。
-        }
+        FirebaseDatabase.getInstance().getReference("users")
+                .child(account).child("password")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        // get password from firebase
+                        String fbPwd = (String) snapshot.getValue();
+                        // 登入成功
+                        if (fbPwd!=null && fbPwd.equals(pwd)){
+                            Log.d("LoginActivity","onDataChange: "+fbPwd);
+                            setResult(RESULT_OK);
+                            finish();//結束此activity,回到上一層。
+                        }else {
+                            // 登入失敗
+                            new AlertDialog.Builder(LoginActivity.this)
+                                    .setTitle("登入結果")
+                                    .setMessage("登入失敗")
+                                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    edAccount.setText("");
+                                                    edPwd.setText("");
+                                                }
+                                            }
+                                    )
+                                    .show();
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+
+//        if (a.equals("hi") && p.equals("1234")) {
+//            setResult(RESULT_OK);
+//            finish();//結束此activity,回到上一層。
+//        }
 
     }
 
