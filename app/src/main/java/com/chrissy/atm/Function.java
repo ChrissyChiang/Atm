590a3bab8f64e26fb4193127af5af8a5cd6089d8
aliencindy <lucy8611892@gmail.com>
2023-03-17 20:40:18 +0800

    格狀有圖示的主畫面

--- app/src/main/java/com/chrissy/atm/Function.java
+++ app/src/main/java/com/chrissy/atm/Function.java
@@ -3,12 +3,26 @@ package com.chrissy.atm;
 public class Function {
 
     String name;
+    int icon;
 
     public Function() {
     }
 
+    public Function(String name, int icon) {
+        this.name = name;
+        this.icon = icon;
+    }
+
     public Function(String name) {
         this.name = name;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
     }
 
     public String getName() {
--- app/src/main/java/com/chrissy/atm/MainActivity.java
+++ app/src/main/java/com/chrissy/atm/MainActivity.java
@@ -2,15 +2,25 @@ package com.chrissy.atm;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     boolean logon = false;
     public static int REQUEST_LOGIN = 100;
+    private List<Function> functions;
 
 
     @Override
@@ -24,15 +34,64 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // Recycler
+        setUpFunctions();
+
         RecyclerView recyclerView = findViewById(R.id.recycler);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new GridLayoutManager(this,4));
 
         // Adapter
-        FunctionAdapter adapter = new FunctionAdapter(this);
+//        FunctionAdapter adapter = new FunctionAdapter(this);
+        IconAdapter adapter = new IconAdapter();
         recyclerView.setAdapter(adapter);
 
 
+    }
+
+    private void setUpFunctions() {
+        functions = new ArrayList<>();
+        String[] funcs= getResources().getStringArray(R.array.functions);
+        functions.add(new Function(funcs[0],R.drawable.func_finance));
+        functions.add(new Function(funcs[1],R.drawable.func_balance));
+        functions.add(new Function(funcs[2],R.drawable.func_invesment));
+        functions.add(new Function(funcs[3],R.drawable.func_contacts));
+        functions.add(new Function(funcs[4],R.drawable.func_exit));
+    }
+
+
+    public class IconAdapter extends RecyclerView.Adapter<IconAdapter.IconHolder>{
+
+
+        @NonNull
+        @Override
+        public IconHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(
+                    R.layout.item_icon,parent,false);
+            return new IconHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull IconHolder holder, int position) {
+            holder.nameText.setText(functions.get(position).getName());
+            holder.iconImage.setImageResource(functions.get(position).getIcon());
+        }
+
+        @Override
+        public int getItemCount() {
+            return functions.size();
+        }
+
+        public class IconHolder extends RecyclerView.ViewHolder{
+            ImageView iconImage;
+            TextView nameText;
+
+            public IconHolder(@NonNull View itemView) {
+                super(itemView);
+                 iconImage= itemView.findViewById(R.id.item_icon);
+                 nameText = itemView.findViewById(R.id.item_name);
+
+            }
+        }
     }
 
     @Override
