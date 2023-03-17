2ef45ce92e8872572da1ddd27e8a03a01ab2ab3a
aliencindy <lucy8611892@gmail.com>
2023-03-18 01:07:09 +0800

    init

--- app/src/main/java/com/chrissy/atm/LoginActivity.java
+++ app/src/main/java/com/chrissy/atm/LoginActivity.java
@@ -10,37 +10,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText edAccount;
-    private EditText edPwd;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_login);
-
-        edAccount = findViewById(ed_account);
-        edPwd = findViewById(R.id.ed_pwd);
-
-    }
-
-
-    public void login(View view) {
-        String a = edAccount.getText().toString();
-        String p = edPwd.getText().toString();
-
-        if (a.equals("hi") && p.equals("1234")) {
-            setResult(RESULT_OK);
-            finish();//結束此activity,回到上一層。
-        }else {
-            setResult(MainActivity.REQUEST_LOGIN);
-            finish();
-        }
-
-    }
-
-    public void cancel(View view) {
-
-    }
 }
--- app/src/main/java/com/chrissy/atm/MainActivity.java
+++ app/src/main/java/com/chrissy/atm/MainActivity.java
@@ -1,41 +1,19 @@
 package com.chrissy.atm;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    boolean logon = false;
-    public static int REQUEST_LOGIN = -100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (!logon) {
-            Intent intent = new Intent(this, LoginActivity.class);
 
-            ActivityResultLauncher<Intent> launcher = registerForActivityResult(
-                    new ActivityResultContracts.StartActivityForResult(),
-                    result -> {
-                        if (result.getResultCode() == RESULT_OK) {
-
-                            if (result.getResultCode() != REQUEST_LOGIN) {
-                                logon = true;
-                            }
-                        } else {
-                            finish();
-                        }
-
-                    });
-
-            launcher.launch(intent);
         }
 
 
     }
-}
