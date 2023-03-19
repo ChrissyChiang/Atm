5498eeda53555063c8d4dabf22ed4824f2c07f43
aliencindy <lucy8611892@gmail.com>
2023-03-19 12:38:23 +0800

    清單展示所有聯絡人

--- app/src/main/java/com/chrissy/atm/ContactActivity.java
+++ app/src/main/java/com/chrissy/atm/ContactActivity.java
@@ -6,11 +6,19 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -35,11 +43,15 @@ public class ContactActivity extends AppCompatActivity {
     private void readContacts() {
         Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI
                 , null, null, null, null);
+
+        List<Contact> contactList = new ArrayList<>();
         while (cursor.moveToNext()) {
             //取得聯絡人ID
             int contactId = cursor.getInt(cursor.getColumnIndexOrThrow(ContactsContract.Contacts.NAME_RAW_CONTACT_ID));
             //取得聯絡人姓名
             String contactName = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME));
+
+            Contact contact = new Contact(contactId, contactName);
 
             //取得聯絡人是否有電話 1有 0沒有
             int contactHasPhone = cursor.getInt(cursor.getColumnIndexOrThrow(ContactsContract.Contacts.HAS_PHONE_NUMBER));
@@ -52,14 +64,64 @@ public class ContactActivity extends AppCompatActivity {
                 while (cursorPhone.moveToNext()) {
                     String contactPhone = cursorPhone.getString(cursorPhone.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER));
                     Log.d(TAG, "readContacts: " + contactId + " " + contactName + " " + contactPhone);
+                    contact.getPhones().add(contactPhone);
                 }
             } else {
                 Log.d(TAG, "readContacts: " + contactId + " " + contactName);
             }
+            contactList.add(contact);
+        }
+        ContactAdapter adapter = new ContactAdapter(contactList);
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
 
+    public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactHolder> {
+        List<Contact> contactList;
 
+        public ContactAdapter(List<Contact> contactList) {
+            this.contactList = contactList;
+        }
+
+        @NonNull
+        @Override
+        public ContactHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(android.R.layout.simple_list_item_2, parent, false);
+            return new ContactHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ContactHolder holder, int position) {
+            Contact contact = contactList.get(position);
+            holder.contactName.setText(contact.getName());
+            StringBuilder sb = new StringBuilder();
+            for (String phone : contact.getPhones()) {
+                sb.append(phone);
+                sb.append(" ");
+            }
+            holder.contactPhone.setText(sb.toString());
+        }
+
+        @Override
+        public int getItemCount() {
+            return contactList.size();
+        }
+
+        public class ContactHolder extends RecyclerView.ViewHolder {
+
+            TextView contactName;
+            TextView contactPhone;
+
+            public ContactHolder(@NonNull View itemView) {
+                super(itemView);
+                contactName = itemView.findViewById(android.R.id.text1);
+                contactPhone = itemView.findViewById(android.R.id.text2);
+            }
         }
     }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
